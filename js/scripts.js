c07b5e352d49f5ca3bce1fa770e97c58c8bfea92
zaman20 <adorzaman18@gmail.com>
2023-02-07 22:58:30 +0600

    sidebar menu add and search bar add and some css fix

---
+++ js/scripts.js
@@ -0,0 +1,32 @@
+$('document').ready(function(){
+    $('.open-menu').on('click',function(){
+        $('.sidebar').addClass('show');
+    });
+    
+    $('.close-icon').on('click',function(){
+        $('.sidebar').removeClass('show');
+    });
+    
+    $('.search-icon').on('click',function(){
+       // alert('hi');
+       $('.search-bar').addClass('search-come');
+       $('.search-icon-off').show();
+       $('.search-icon').hide();
+    })
+
+    $('.search-icon-off').on('click',function(){
+        // alert('hi');
+        $('.search-bar').removeClass('search-come');
+        $('.search-icon-off').hide();
+        $('.search-icon').show();
+     })
+    
+
+
+
+
+
+
+
+    // end
+});
